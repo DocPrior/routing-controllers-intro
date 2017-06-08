58cd682e5ba1d9533e8ce48299a37f7db67aa257
DocPrior <Sara Prior>
2017-06-08 14:12:35 -0400

    added header instance variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is a welcome page."
+
     render :welcome
   end
 
   def about
-    
+    @header = "This is an about page."
+  end
+
+  def contest
+    @heaeder = "This is a contest page?"
   end
 
 end
